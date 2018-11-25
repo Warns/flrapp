f897da6a31737a70226d5b67dd5045f78927500f
burakkarakaya <Brk@BlackRocks-iMac.local>
2018-11-25 21:44:41 +0300

    sepet sayfası adres düzenlemeler

--- app/helper/Translation.js
+++ app/helper/Translation.js
@@ -19,12 +19,13 @@ module.exports = {
         edit: 'Düzenle',
         select: 'SEÇ',
         selected: 'SEÇİLDİ',
-        selectShipAddress: 'ADRESİ SEÇ',
-        selectedShipAddress: 'SEÇİLİ ADRES',
+        selectShipAddress: 'TESLİMAT ADRESİ',
+        selectedShipAddress: 'TESLİMAT ADRESİ',
         selectBillAddress: 'FATURA ADRESİ',
-        selectedBillAddress: 'FATURA ADRESİ SEÇİLDİ',
+        selectedBillAddress: 'FATURA ADRESİ',
         errorShipAddress: 'Teslimat ve fatura adresi seçmelisiniz.',
         errorBillAddress: 'Fatura adresi seçmelisiniz.',
+        errorCargo: 'Kargo seçmelisiniz'
     },
     store: {
         headerTitle: 'YAKIN MAĞAZALAR'
