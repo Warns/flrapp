5dfc65deefc0662b589340739897efa3de548682
Alaa Alnuaimi <alacaesar@gmail.com>
2018-11-28 05:43:21 +0300

    conflict fix

--- app/viewer/cart/Footer.js
+++ app/viewer/cart/Footer.js
@@ -124,160 +124,158 @@ class Main extends Component {
       const couponCode = _self._getCouponCode();
       if (couponCode != "") txt = "Bu promosyon kodunu girdiniz:";
     }
+  };
 
-    _getCouponCode = () => {
-      const _self = this,
-        { cartInfo = {} } = _self.props.cart,
-        { couponCode = null } = cartInfo;
+  _getCouponCode = () => {
+    const _self = this,
+      { cartInfo = {} } = _self.props.cart,
+      { couponCode = null } = cartInfo;
 
-      return couponCode || "";
-    };
+    return couponCode || "";
+  };
 
-    _getForm = () => {
-      const _self = this,
-        { showCoupon = false } = _self.state,
-        { coupon = false } = _self.props.data;
-      let view = null;
-      if (coupon && showCoupon) {
-        const couponCode = _self._getCouponCode(),
-          data =
-            couponCode != "" ? FORMDATA["deleteCoupon"] : FORMDATA["useCoupon"],
-          ico = couponCode != "" ? "searchClose" : "button",
-          passive =
-            couponCode != "" ? (
-              <View
-                style={{
-                  width: "100%",
-                  height: "100%",
-                  position: "absolute",
-                  left: 0,
-                  top: 0,
-                  zIndex: 1
-                }}
-              />
-            ) : null;
-
-        data["fields"][0]["items"][0]["value"] = couponCode;
-
-        view = (
-          <View
-            style={{ flexDirection: "row", marginTop: 10, marginBottom: 10 }}
-          >
-            <Form
-              onRef={ref => (_self.child = ref)}
-              scrollEnabled={false}
-              style={{ paddingLeft: 0, paddingRight: 0, paddingBottom: 0 }}
-              data={data}
-              callback={this._onFormCallback}
-            />
-            {passive}
-            <IconButton
-              style={{ zIndex: 5, position: "absolute", right: 5, top: 5 }}
-              ico={ico}
-              callback={_self._onCouponButton}
-            />
-          </View>
-        );
-      }
-
-      return view;
-    };
-
-    _getFormButton = () => {
-      const _self = this,
-        { cartNoResult = false } = _self.props.cart,
-        { showCoupon = false } = _self.state,
-        { coupon = false } = _self.props.data,
-        ico = showCoupon ? "upArrow" : "bottomArrow";
-
-      let txt = "Promosyon Kodu";
-      if (showCoupon) {
-        const couponCode = _self._getCouponCode();
-        if (couponCode != "") txt = "Bu promosyon kodunu girdiniz:";
-      }
-
-      let view = null;
-      if (coupon && !cartNoResult)
-        view = (
-          <TouchableOpacity
-            style={{
-              flexDirection: "row",
-              justifyContent: "center",
-              alignItems: "center"
-            }}
-            activeOpacity={0.8}
-            onPress={_self._onShowCoupon}
-          >
-            <Text
+  _getForm = () => {
+    const _self = this,
+      { showCoupon = false } = _self.state,
+      { coupon = false } = _self.props.data;
+    let view = null;
+    if (coupon && showCoupon) {
+      const couponCode = _self._getCouponCode(),
+        data =
+          couponCode != "" ? FORMDATA["deleteCoupon"] : FORMDATA["useCoupon"],
+        ico = couponCode != "" ? "searchClose" : "button",
+        passive =
+          couponCode != "" ? (
+            <View
               style={{
-                fontFamily: "RegularTyp2",
-                fontSize: 13,
-                marginRight: 12
+                width: "100%",
+                height: "100%",
+                position: "absolute",
+                left: 0,
+                top: 0,
+                zIndex: 1
               }}
-            >
-              {txt}
-            </Text>
-            <Image style={{ width: 9, height: 5 }} source={ICONS[ico]} />
-          </TouchableOpacity>
-        );
-      return view;
-    };
+            />
+          ) : null;
 
-    _getFoot = () => {
-      const _self = this,
-        { showCoupon = false } = _self.state,
-        { coupon = false } = _self.props.data,
-        { cartInfo = {} } = _self.props.cart,
-        { subTotal = 0, discountTotal = 0, netTotal = 0 } = cartInfo,
-        form = _self._getForm(),
-        formButton = _self._getFormButton(),
-        prc = (
-          <View style={{ alignItems: "flex-end" }}>
-            <Text style={{ fontFamily: "Bold", fontSize: 15 }}>
-              TOPLAM: {Utils.getPriceFormat(netTotal)}
-            </Text>
-            <View style={{ flexDirection: "row" }}>
-              <Text style={{ fontFamily: "RegularTyp2", fontSize: 13 }}>
-                İNDİRİM: {Utils.getPriceFormat(discountTotal)},{" "}
-              </Text>
-              <Text style={{ fontFamily: "RegularTyp2", fontSize: 13 }}>
-                ARA TOPLAM: {Utils.getPriceFormat(subTotal)}
-              </Text>
-            </View>
-          </View>
-        );
+      data["fields"][0]["items"][0]["value"] = couponCode;
 
-      let view = (
-        <View
+      view = (
+        <View style={{ flexDirection: "row", marginTop: 10, marginBottom: 10 }}>
+          <Form
+            onRef={ref => (_self.child = ref)}
+            scrollEnabled={false}
+            style={{ paddingLeft: 0, paddingRight: 0, paddingBottom: 0 }}
+            data={data}
+            callback={this._onFormCallback}
+          />
+          {passive}
+          <IconButton
+            style={{ zIndex: 5, position: "absolute", right: 5, top: 5 }}
+            ico={ico}
+            callback={_self._onCouponButton}
+          />
+        </View>
+      );
+    }
+
+    return view;
+  };
+
+  _getFormButton = () => {
+    const _self = this,
+      { cartNoResult = false } = _self.props.cart,
+      { showCoupon = false } = _self.state,
+      { coupon = false } = _self.props.data,
+      ico = showCoupon ? "upArrow" : "bottomArrow";
+
+    let txt = "Promosyon Kodu";
+    if (showCoupon) {
+      const couponCode = _self._getCouponCode();
+      if (couponCode != "") txt = "Bu promosyon kodunu girdiniz:";
+    }
+
+    let view = null;
+    if (coupon && !cartNoResult)
+      view = (
+        <TouchableOpacity
           style={{
             flexDirection: "row",
-            width: "100%",
-            paddingTop: 10,
-            paddingBottom: 10
+            justifyContent: "center",
+            alignItems: "center"
           }}
+          activeOpacity={0.8}
+          onPress={_self._onShowCoupon}
         >
-          <View style={{ width: "50%", alignItems: "flex-start" }}>
-            {formButton}
+          <Text
+            style={{
+              fontFamily: "RegularTyp2",
+              fontSize: 13,
+              marginRight: 12
+            }}
+          >
+            {txt}
+          </Text>
+          <Image style={{ width: 9, height: 5 }} source={ICONS[ico]} />
+        </TouchableOpacity>
+      );
+    return view;
+  };
+
+  _getFoot = () => {
+    const _self = this,
+      { showCoupon = false } = _self.state,
+      { coupon = false } = _self.props.data,
+      { cartInfo = {} } = _self.props.cart,
+      { subTotal = 0, discountTotal = 0, netTotal = 0 } = cartInfo,
+      form = _self._getForm(),
+      formButton = _self._getFormButton(),
+      prc = (
+        <View style={{ alignItems: "flex-end" }}>
+          <Text style={{ fontFamily: "Bold", fontSize: 15 }}>
+            TOPLAM: {Utils.getPriceFormat(netTotal)}
+          </Text>
+          <View style={{ flexDirection: "row" }}>
+            <Text style={{ fontFamily: "RegularTyp2", fontSize: 13 }}>
+              İNDİRİM: {Utils.getPriceFormat(discountTotal)},{" "}
+            </Text>
+            <Text style={{ fontFamily: "RegularTyp2", fontSize: 13 }}>
+              ARA TOPLAM: {Utils.getPriceFormat(subTotal)}
+            </Text>
           </View>
-          <View style={{ width: "50%", alignItems: "flex-end" }}>{prc}</View>
         </View>
       );
 
-      if (coupon && showCoupon)
-        view = (
-          <View style={{ width: "100%", paddingTop: 10, paddingBottom: 10 }}>
-            <View style={{ width: "100%", alignItems: "flex-start" }}>
-              {formButton}
-            </View>
-            <View style={{ width: "100%", paddingLeft: 20, paddingRight: 20 }}>
-              {form}
-            </View>
-            <View style={{ width: "100%", alignItems: "flex-end" }}>{prc}</View>
-          </View>
-        );
+    let view = (
+      <View
+        style={{
+          flexDirection: "row",
+          width: "100%",
+          paddingTop: 10,
+          paddingBottom: 10
+        }}
+      >
+        <View style={{ width: "50%", alignItems: "flex-start" }}>
+          {formButton}
+        </View>
+        <View style={{ width: "50%", alignItems: "flex-end" }}>{prc}</View>
+      </View>
+    );
 
-      return view;
-    };
+    if (coupon && showCoupon)
+      view = (
+        <View style={{ width: "100%", paddingTop: 10, paddingBottom: 10 }}>
+          <View style={{ width: "100%", alignItems: "flex-start" }}>
+            {formButton}
+          </View>
+          <View style={{ width: "100%", paddingLeft: 20, paddingRight: 20 }}>
+            {form}
+          </View>
+          <View style={{ width: "100%", alignItems: "flex-end" }}>{prc}</View>
+        </View>
+      );
+
+    return view;
   };
 
   render() {
